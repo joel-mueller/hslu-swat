bc942d65f76b21aada4bd12ad23c0ba81b92820a
Joel Mueller <joelmueller27@gmail.com>
2025-03-06 13:46:56 +0100

    Initialize Spring Boot

---
+++ src/main/java/ch/hslu/api/Api.java
@@ -0,0 +1,14 @@
+package ch.hslu.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Api {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Spring Boot!";
+    }
+}
