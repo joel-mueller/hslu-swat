822b59646d8f45ea02f371a7977993a8e9772562
Jae <39260243+Spoicy@users.noreply.github.com>
2025-03-27 00:46:00 +0100

    Add sql to existing DatabaseConnector methods

--- src/main/java/ch/hslu/persistence/DatabaseConnector.java
+++ src/main/java/ch/hslu/persistence/DatabaseConnector.java
@@ -45,15 +45,51 @@ public class DatabaseConnector implements Database {
 
     @Override
     public Customer getCustomer(UUID id) {
-        return new Customer(id, "Max", "Mustermann", "Stree1", "3355");
+        PreparedStatement stmt;
+        try {
+            stmt = connection.prepareStatement("SELECT * FROM customers WHERE id = ?");
+            stmt.setString(1, id.toString());
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return new Customer(id,
+                        rs.getString("first_name"),
+                        rs.getString("last_name"),
+                        rs.getString("street"),
+                        rs.getString("zip_code"));
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     // TODO updateCustomer() deleteCustomer() addCustomer()
 
     @Override
     public Book getBook(int id) {
-        return new Book(id, "123412341234", "Hello Title", "John", "2015", "Exlibris", "ImageUrl", "Image url medium",
-                "Image url large");
+        PreparedStatement stmt;
+        try {
+            stmt = connection.prepareStatement("SELECT * FROM books WHERE id = ?");
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return new Book(rs.getInt("id"),
+                        rs.getString("isbn"),
+                        rs.getString("title"),
+                        rs.getString("author"),
+                        rs.getString("year"),
+                        rs.getString("publisher"),
+                        rs.getString("imageUrlS"),
+                        rs.getString("imageUrlM"),
+                        rs.getString("imageUrlL"));
+            } else {
+                return null;
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     // TODO updateBook() deleteBook() (maybe also retire instead of delete) addBook()
@@ -65,11 +101,35 @@ public class DatabaseConnector implements Database {
 
     @Override
     public boolean addBorrowRecord(BorrowRecord record) {
-        return true;
+        PreparedStatement stmt;
+        try {
+            stmt = connection.prepareStatement("INSERT INTO borrow_records(uuid, book_id, customer_id, date_borrowed, duration_days, borrowed) VALUES(?,?,?,?,?,?)");
+            stmt.setString(1, record.id().toString());
+            stmt.setInt(2, record.bookId());
+            stmt.setString(3, record.customerId().toString());
+            stmt.setString(4, record.dateBorrowed().toString());
+            stmt.setInt(5, record.duration().getDays());
+            stmt.setBoolean(6, record.returned());
+            return stmt.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public boolean updateBorrowRecord(BorrowRecord record) {
-        return true;
+        PreparedStatement stmt;
+        try {
+            stmt = connection.prepareStatement("UPDATE borrow_records SET book_id = ?, customer_id ?, date_borrowed = ?, duration = ?, returned = ? WHERE id = ?");
+            stmt.setInt(1, record.bookId());
+            stmt.setString(2, record.customerId().toString());
+            stmt.setString(3, record.dateBorrowed().toString());
+            stmt.setInt(4, record.duration().getDays());
+            stmt.setBoolean(5, record.returned());
+            stmt.setString(6, record.id().toString());
+            return stmt.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
